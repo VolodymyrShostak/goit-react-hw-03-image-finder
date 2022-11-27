2ce12ba946b195d3b0d27aa398aeb24013e3c61b
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-11-28 00:06:24 +0200

    add proptypes

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import {Overlay,ModalView} from './styled'
+import { Overlay, ModalView } from './styled'
+import PropTypes from 'prop-types'; 
 
 export const Modal = ({ url, closeModal=()=>{} }) => {
   return (
     <Overlay>
-          <ModalView closeModal={closeModal()}>
+      <ModalView onClick={closeModal}>
         <img src={url} alt="" />
       </ModalView>
     </Overlay>
   );
 };
+
+Modal.propTypes = {
+    url: PropTypes.string,
+    closeModal: PropTypes.func,
+}
